572ac6150051f51f92a00254c8b8613ee45bdf15
oscaruiz <oscarruizf@gmail.com>
2025-06-16 10:31:59 +0200

    Implements Cleaner Architecture

---
+++ src/main/java/com/oscaruiz/mycqrs/core/domain/event/EventBus.java
@@ -0,0 +1,22 @@
+package com.oscaruiz.mycqrs.core.domain.event;
+/**
+ * Interface for publishing and subscribing to domain events.
+ */
+public interface EventBus {
+
+    /**
+     * Publishes an event to all registered handlers.
+     *
+     * @param event the event to publish
+     */
+    <T extends Event> void publish(T event);
+
+    /**
+     * Registers a handler for a specific event type.
+     *
+     * @param eventType the type of event
+     * @param handler the handler for that event type
+     * @param <EventType> the event class
+     */
+    <EventType extends Event> void registerHandler(Class<EventType> eventType, EventHandler<EventType> handler);
+}
