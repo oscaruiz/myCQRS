29162feb1dd26d480c66699b4a6b356fe00cf159
oscaruiz <oscarruizf@gmail.com>
2026-04-02 11:25:52 +0200

    feat: add Spring profiles for dev and test environments

--- src/demo/src/test/java/com/oscaruiz/mycqrs/demo/integration/CommandQuerySmokeIntegrationTest.java
+++ src/demo/src/test/java/com/oscaruiz/mycqrs/demo/integration/CommandQuerySmokeIntegrationTest.java
@@ -11,25 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = CommandQuerySmokeIntegrationTest.TestConfig.class)
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:smoke;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
-        "spring.datasource.driverClassName=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.show-sql=false"
-})
+@ActiveProfiles("test")
 class CommandQuerySmokeIntegrationTest {
 
     @Autowired
@@ -59,7 +52,10 @@ class CommandQuerySmokeIntegrationTest {
             "com.oscaruiz.mycqrs.demo.application",
             "com.oscaruiz.mycqrs.demo.domain",
             "com.oscaruiz.mycqrs.demo.infrastructure"
-    })
+    }, excludeFilters = @ComponentScan.Filter(
+            type = org.springframework.context.annotation.FilterType.REGEX,
+            pattern = "com\\.oscaruiz\\.mycqrs\\.demo\\.infrastructure\\.mongo\\..*"
+    ))
     @EnableJpaRepositories(basePackages = "com.oscaruiz.mycqrs.demo.infrastructure.jpa")
     @EntityScan(basePackageClasses = BookEntity.class)
     static class TestConfig {
